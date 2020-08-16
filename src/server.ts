571ac8b8b15298a5c3b922e43e1ba0b148d5014c
danielferreiradf <danielferreiradk@gmail.com>
2020-08-16 20:46:53 +0100

    chore: added typeorm to app

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import './database';
+
 const app = express();
 
+app.use(express());
+
 app.listen(5000, () => {
-  console.log('Server running! 🐱‍🏍');
+  console.log('Server running!');
 });
