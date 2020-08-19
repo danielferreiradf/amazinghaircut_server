1f32e964fff8969e32333d537e9dceb77e19fef3
danielferreiradf <danielferreiradk@gmail.com>
2020-08-19 10:44:28 +0100

    feat: added create appointment routes service

--- src/database/migrations/1597607435668-CreateAppointments.ts
+++ src/database/migrations/1597607435668-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1597607435668
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import 'reflect-metadata';
+
 import express from 'express';
+import routes from './routes';
 
 import './database';
 
 const app = express();
 
-app.use(express());
+app.use(express.json());
+app.use(routes);
 
 app.listen(5000, () => {
-  console.log('Server running!');
+  console.log('Server running on port 5000!');
 });
